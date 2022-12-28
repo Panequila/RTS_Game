8d04a8efb096bfa7083979bd958c5b3937e38422
Ramez <ramezmmbell@gmail.com>
2022-12-28 15:08:36 +0200

    Removed "Started Content"
    "Started Content" file that came with a blank project was too large to upload to github, so I deleted it.

---
+++ Source/RTS_Game/PlayerPawn.cpp
@@ -0,0 +1,104 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Instantiating the camera here in the class didn't make the rotation work properly, the camera was the object that rotated not the spring arm.
+	//I had to create it in the blueprint and do the coding here.
+
+	#pragma region Camera Creation Code
+	// RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("Root Scene"));
+	// RootComponent = RootScene;
+
+	// SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	// SpringArmComp->SetupAttachment(RootScene);
+	// SpringArmComp->bDoCollisionTest = false;
+	// //Setting the starting rotation.
+	// //SpringArmComp->SetRelativeRotation(FRotator(-50,0,0));
+
+	// //SpringArmComp->bUsePawnControlRotation = true;
+	// SpringArmComp->bEnableCameraLag = true;
+	// SpringArmComp->TargetArmLength = 300.0f;
+
+
+	// CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	// CameraComp->AttachToComponent(SpringArmComp, FAttachmentTransformRules::KeepRelativeTransform);
+
+	#pragma endregion
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+	// FRotator newYaw = GetActorRotation();
+	// FRotator newPitch = SpringArmComp->GetComponentRotation();
+	// newYaw.Yaw += mouseInput.X;
+	// newPitch.Pitch += mouseInput.Y;
+	// SetActorRotation(newYaw);
+	// SpringArmComp->SetWorldRotation(newPitch);
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &APlayerPawn::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &APlayerPawn::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("RotateCamera"), this, &APlayerPawn::RotateCamera);
+
+	// PlayerInputComponent->BindAxis(TEXT("MouseYaw"), this, &APlayerPawn::MouseYaw);
+	// PlayerInputComponent->BindAxis(TEXT("MousePitch"), this, &APlayerPawn::MousePitch);
+
+
+
+}
+
+void APlayerPawn::MoveForward(float AxisValue)
+{
+	//getting the forward value of the pawn
+	UE_LOG(LogTemp, Warning, TEXT("The float value is: %f"), AxisValue);
+	//MovementDirection.X = FMath::Clamp(AxisValue, -1.0f, 1.0f);
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+void APlayerPawn::MoveRight(float AxisValue)
+{
+	//getting the right value of the pawn
+	//MovementDirection.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f);
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
+void APlayerPawn::RotateCamera(float AxisValue)
+{
+	//AddActorLocalRotation(FRotator(0, AxisValue, 0));
+	//SetActorRotation(GetActorRotation(),FRotator(0,AxisValue,0));
+	//AddMovementInput(GetActorRotationVector(),AxisValue*500);
+
+	//Changing the Yaw value according the rotation input.
+	FRotator NewRotation = FRotator(0.f,AxisValue,0.f);
+	//Turning the Euler rotation to Quternion.
+	FQuat QuatRotation = FQuat(NewRotation);
+	AddActorLocalRotation(QuatRotation, false, 0, ETeleportType::None);
+	
+}
+
---
+++ Source/RTS_Game/PlayerPawn.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+class USceneComponent;
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class RTS_GAME_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// UPROPERTY()
+	// USceneComponent* RootScene;
+
+	// UPROPERTY()
+	// USpringArmComponent* SpringArmComp;
+
+	// UPROPERTY()
+	// UCameraComponent* CameraComp;
+
+	FVector MovementDirection;
+	FVector RotationDirection;
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void RotateCamera(float AxisValue); 
+
+	// void MouseYaw(float axis);
+	// void MousePitch(float axis);
+	// FVector2D mouseInput;
+
+};
